f3e7db134234026fb44d75d4247bdb4d6d8dafcc
Martin Sonnberger <msonnberger.mmt-b2020@fh-salzburg.ac.at>
2021-05-24 18:03:14 +0200

    added statistics page

--- client/statistics/index.php
+++ client/statistics/index.php
@@ -8,38 +8,62 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="../favicon.ico" type="image/x-icon" />
+    <link rel="stylesheet" href="../css/normalize.css" />
+    <link rel="stylesheet" href="../css/styles.css" />
+    <link rel="stylesheet" href="../css/stats-styles.css" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Source+Code+Pro:wght@300;400;500;700&family=Source+Sans+Pro:wght@300;400;700&display=swap"
+      rel="stylesheet"
+    />
     <title>Statistiken</title>
 </head>
 <body>
-    <table class="stats-table">
-        <tr>
-            <th>Punkte</th>
-            <th>In-Modus</th>
-            <th>Out-Modus</th>
-            <th>Runden Spieler 1</th>
-            <th>Runden Spieler 2</th>
-            <th>Avg Spieler 1</th>
-            <th>Avg Spieler 2</th>
-        </tr>
-
-        <?php
-        $sth = $dbh->query("SELECT * FROM game;");
-        $games = $sth->fetchAll();
-
-        foreach ($games as $game) {
-            ?>
-            <tr>
-                <td><?= $game['point_mode']     ?></td>
-                <td><?= $game['in_mode']        ?></td>
-                <td><?= $game['out_mode']       ?></td>
-                <td><?= $game['rounds_player1'] ?></td>
-                <td><?= $game['rounds_player2'] ?></td>
-                <td><?= $game['avg_player1']    ?></td>
-                <td><?= $game['avg_player2']    ?></td>
+    <header class="game-header">
+        <a href="/MMP1/client/"><img src="../img/logo.svg" alt="darts.io Logo" class="logo" /></a>
+    </header>
+    <main>
+        <table class="stats-table">
+            <tr class="table-header">
+                <th class="text-cell">Punkte</th>
+                <th class="text-cell">In-Modus</th>
+                <th class="text-cell">Out-Modus</th>
+                <th colspan="2">Runden</th>
+                <th colspan="2">Average</th>
             </tr>
-        <?php
-        }
-        ?>
-    </table>
+            <tr class="table-header">
+                <td></td>
+                <td></td>
+                <td></td>
+                <td class="number-cell player-label">Spieler 1</td>
+                <td class="number-cell player-label">Spieler 2</td>
+                <td class="number-cell player-label">Spieler 1</td>
+                <td class="number-cell player-label">Spieler 2</td>
+            </tr>
+            <?php
+            $sth = $dbh->query("SELECT * FROM game ORDER BY avg_player1 DESC LIMIT 100;");
+            $games = $sth->fetchAll();
+            foreach ($games as $game) {
+                ?>
+                <tr>
+                    <td class="text-cell">  <?= $game['point_mode']     ?></td>
+                    <td class="text-cell">  <?= $game['in_mode']        ?></td>
+                    <td class="text-cell">  <?= $game['out_mode']       ?></td>
+                    <td class="number-cell"><?= $game['rounds_player1'] ?></td>
+                    <td class="number-cell"><?= $game['rounds_player2'] ?></td>
+                    <td class="number-cell"><?= $game['avg_player1']    ?></td>
+                    <td class="number-cell"><?= $game['avg_player2']    ?></td>
+                </tr>
+            <?php
+            }
+            ?>
+        </table>
+    </main>
+
+    <footer>
+        <p class="footer-text">© Martin Sonnberger 2021</p>
+        <a href="imprint" class="footer-link">Impressum</a>
+    </footer>
 </body>
 </html>
